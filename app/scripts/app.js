b8ed66f6c80252807e5f0fdc4a1722e357a7c7e2
Dominic Czarnota <dczarno@gmail.com>
2015-03-20 16:37:30 -0700

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name wordScrambleGameApp
+ * @description
+ * # wordScrambleGameApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('wordScrambleGameApp', []);
